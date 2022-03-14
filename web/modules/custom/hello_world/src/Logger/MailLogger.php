eddc1914f4c8c0f75d9492c2f2d7dd6c4d767585
Ada Hernandez <hadageorgina@gmail.com>
2022-03-14 16:31:01 -0600

    config changes

--- web/modules/custom/hello_world/src/Logger/MailLogger.php
+++ web/modules/custom/hello_world/src/Logger/MailLogger.php
@@ -2,7 +2,11 @@
 
 namespace Drupal\hello_world\Logger;
 
+use Drupal\Component\Render\FormattableMarkup;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Logger\LogMessageParserInterface;
 use Drupal\Core\Logger\RfcLoggerTrait;
+use Drupal\Core\Logger\RfcLogLevel;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -12,10 +16,25 @@ class MailLogger implements LoggerInterface {
   use RfcLoggerTrait;
 
   /**
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   * @param \Drupal\Core\Logger\LogMessageParserInterface $logMessageParser
+   */
+  public function __construct(protected ConfigFactoryInterface $configFactory, protected LogMessageParserInterface $logMessageParser) {}
+
+  /**
    * {@inheritdoc}
    */
   public function log($level, $message, array $context = []) {
-    // TODO: Implement log() method.
+    if ($level !== RfcLogLevel::ERROR) {
+      return;
+    }
+    $to = $this->configFactory->get('system.site')->get('mail');
+    $langcode = $this->configFactory->get('system.site')->get('langcode');
+    $variables = $this->logMessageParser->parseMessagePlaceholders($message, $context);
+    $markup = new FormattableMarkup($message, $variables);
+    \Drupal::service('plugin.manager.mail')->mail('hello_world', 'hello_world_log', $to, $langcode, ['message' => $markup]);//#86.
+
+
   }
 
 }
